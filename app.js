2eab2303b4af25562a2d8bb282e0af987e054bd8
xavimartz <luis.martz7@hotmail.com>
2019-09-19 21:33:06 -0500

    adding node-sass for styles

--- app.js
+++ app.js
@@ -47,6 +47,16 @@ app.use(
   })
 );
 
+// Express View engine setup
+
+app.use(
+  require('node-sass-middleware')({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    sourceMap: true
+  })
+)
+
 // Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
